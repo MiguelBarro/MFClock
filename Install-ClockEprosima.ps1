d633b344d725423902bae5e0d2fb94b30c5a0a7b
Miguel Barro <miguelbarro@eprosima.com>
2021-01-07 10:21:26 +0100

    Solving issues with foonathan current master

--- Install-ClockEprosima.ps1
+++ Install-ClockEprosima.ps1
@@ -1,4 +1,4 @@
-<# 
+<#
 
 .SYNOPSIS
 
@@ -7,7 +7,7 @@ Build & install eProsima Clock example
 .DESCRIPTION
 
 Install-ClockEprosima uses CMake setup with the given parameters to build & install this example and all its
-dependencies  
+dependencies
 
 .PARAMETER InstallDir
 
@@ -15,14 +15,14 @@ Directory where the example and its dependencies will be installed
 
 .PARAMETER BuildDir
 
-Directory where the example and its dependencies will be cloned and built 
+Directory where the example and its dependencies will be cloned and built
 
-.PARAMETER DebugBuild 
+.PARAMETER DebugBuild
 
-Use the flag -DebugBuild to generate a debug build. 
+Use the flag -DebugBuild to generate a debug build.
 
 .PARAMETER SharedLibs
-Specify if static or dynamic libraries should be generated 
+Specify if static or dynamic libraries should be generated
 
 .EXAMPLE
 
@@ -34,15 +34,15 @@ libraries. eProsimaClock will be statically link to C++ runtime and MFC.
 .EXAMPLE
 
 This will install the example in Program Files and build in a temporary dir a
-Release version of the example using C++ runtime and MFC as dynamic libraries. 
+Release version of the example using C++ runtime and MFC as dynamic libraries.
 
 > Install-ClockEprosima $Env:ProgramFiles\eProsimaClock $Env:TMP\eProsimaClock -SharedLibs
 
-.LINK 
+.LINK
 
 https://github.com/MiguelBarro/MFClock.git
 
-.LINK 
+.LINK
 
 https://fast-dds.docs.eprosima.com/en/latest/
 
@@ -56,34 +56,34 @@ https://www.eprosima.com/
 Param(
 
 [Parameter(Position=0, Mandatory=$true)]
-[ValidateScript({Test-Path $_ -PathType Container -IsValid })] 
+[ValidateScript({Test-Path $_ -PathType Container -IsValid })]
 [Alias("Install")]
 [String]
 # directory where the example will be installed
 $InstallDir,
 
 [Parameter(Position=1)]
-[ValidateScript({Test-Path $_ -PathType Container -IsValid })] 
+[ValidateScript({Test-Path $_ -PathType Container -IsValid })]
 [Alias("Build")]
 [String]
-# directory where the example will be build 
+# directory where the example will be build
 $BuildDir="$Env:TMP\eProsima" + (New-Guid),
 
 [Switch]
-# use the flag -DebugBuild to generate a debug build. 
+# use the flag -DebugBuild to generate a debug build.
 $DebugBuild,
 
 [Switch]
-# specify if static or dynamic libraries should be generated 
+# specify if static or dynamic libraries should be generated
 $SharedLibs
-    
+
 )
 
 # create the install path if it doesn't exists
-if (!(Test-Path $InstallDir -PathType Container)) { mkdir $InstallDir }
+if (!(Test-Path $InstallDir -PathType Container)) { mkdir $InstallDir | Out-Null }
 
 # create the install path if it doesn't exists
-if (!(Test-Path $BuildDir -PathType Container)) { mkdir $BuildDir }
+if (!(Test-Path $BuildDir -PathType Container)) { mkdir $BuildDir | Out-Null }
 
 # rig SharedLibs
 $build_shared = "-DBUILD_SHARED_LIBS=$(if($SharedLibs) {"ON"} else {"OFF"})"
@@ -98,14 +98,14 @@ $runtime = "-DCMAKE_MSVC_RUNTIME_LIBRARY=`"MultiThreaded$<$<CONFIG:Debug>:Debug>
 pushd
 cd $BuildDir
 
-# fastcdr build ----------------------------- 
+# fastcdr build -----------------------------
 git clone https://github.com/eProsima/Fast-CDR.git
 
 Join-Path $BuildDir Fast-CDR/build -OutVariable fastcdr_build
 
-cmake.exe -DCMAKE_INSTALL_PREFIX="$InstallDir" $build_shared $runtime -DCMAKE_POLICY_DEFAULT_CMP0091=NEW -Ax64 "-B$fastcdr_build" Fast-CDR 
+cmake.exe -DCMAKE_INSTALL_PREFIX="$InstallDir" $build_shared $runtime -DCMAKE_POLICY_DEFAULT_CMP0091=NEW -Ax64 "-B$fastcdr_build" Fast-CDR
 
-cmake --build $fastcdr_build --config $build_config --target install 
+cmake --build $fastcdr_build --config $build_config --target install
 
 # foonatham memory --------------------------
 git clone --recurse-submodules --branch v0.6-2 https://github.com/foonathan/memory.git
@@ -113,18 +113,18 @@ git clone --recurse-submodules --branch v0.6-2 https://github.com/foonathan/memo
 Join-Path $BuildDir memory/build -OutVariable memory_build
 
 # foonathan is always a static lib
-cmake -DCMAKE_INSTALL_PREFIX="$InstallDir" -DBUILD_SHARED_LIBS=OFF $runtime -DCMAKE_POLICY_DEFAULT_CMP0091=NEW -Ax64 "-B$memory_build" memory 
+cmake -DCMAKE_INSTALL_PREFIX="$InstallDir" -DBUILD_SHARED_LIBS=OFF -DFOONATHAN_MEMORY_BUILD_TOOLS=ON -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DFOONATHAN_MEMORY_BUILD_TESTS=OFF $runtime -DCMAKE_POLICY_DEFAULT_CMP0091=NEW -Ax64 "-B$memory_build" memory
 
-cmake --build $memory_build --config $build_config --target install 
+cmake --build $memory_build --config $build_config --target install
 
 # fastdds -----------------------------------
-git clone https://github.com/eProsima/Fast-DDS.git 
+git clone https://github.com/eProsima/Fast-DDS.git
 
 Join-Path $BuildDir Fast-DDS/build -OutVariable fastdds_build
 
-cmake -DCMAKE_INSTALL_PREFIX="$InstallDir" $build_shared $runtime -DCMAKE_POLICY_DEFAULT_CMP0091=NEW -DTHIRDPARTY=ON -DCMAKE_PREFIX_PATH="$InstallDir" -DNO_TLS=ON -DSECURITY=OFF -DCOMPILE_EXAMPLES=OFF -DEPROSIMA_BUILD_TESTS=OFF "-DINTERNAL_DEBUG=$(if($DebugBuild){"ON"})" -Ax64 "-B$fastdds_build" Fast-DDS 
+cmake -DCMAKE_INSTALL_PREFIX="$InstallDir" $build_shared $runtime -DCMAKE_POLICY_DEFAULT_CMP0091=NEW -DTHIRDPARTY=ON -DCMAKE_PREFIX_PATH="$InstallDir" -DNO_TLS=ON -DSECURITY=OFF -DCOMPILE_EXAMPLES=OFF -DEPROSIMA_BUILD_TESTS=OFF "-DINTERNAL_DEBUG=$(if($DebugBuild){"ON"})" -Ax64 "-B$fastdds_build" Fast-DDS
 
-cmake --build $fastdds_build --config $build_config --target install 
+cmake --build $fastdds_build --config $build_config --target install
 
 # eProsimaClock -----------------------------
 git clone https://github.com/MiguelBarro/MFClock.git
@@ -137,9 +137,9 @@ Join-Path $BuildDir MFClock/build -OutVariable clock_build
 # how we want fastdds to be linked (that doesn't match the meaning on CMake).
 # This way we mimic how fastdds is build which simplifies setup.
 
-cmake -DCMAKE_INSTALL_PREFIX="$InstallDir" $build_shared -DCMAKE_PREFIX_PATH="$InstallDir" "-B$clock_build" -Ax64 MFClock 
+cmake -DCMAKE_INSTALL_PREFIX="$InstallDir" $build_shared -DCMAKE_PREFIX_PATH="$InstallDir" "-B$clock_build" -Ax64 MFClock
 
-cmake --build $clock_build --config $build_config --target install 
+cmake --build $clock_build --config $build_config --target install
 
 # restore the original dir
 popd
